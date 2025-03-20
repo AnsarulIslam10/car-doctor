5e7f4ef1fe874d43435a136cff9930b35b479657
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-03-20 23:55:02 +0600

    services card design

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
 import SectionTitle from "./SectionTitle";
+import { BsArrowRight } from "react-icons/bs";
+import Link from "next/link";
 
 const Services = async () => {
   const data = [
@@ -198,8 +201,28 @@ const Services = async () => {
         desc="the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. "
       />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
-        {data.map((singleData) => (
-          <p key={singleData._id}>{singleData.title}</p>
+        {data.map((item) => (
+          <div
+            key={item._id}
+            className="p-6 border rounded-xl border-[#E8E8E8]"
+          >
+            <figure className="w-full h-3/4 flex justify-center items-center">
+              <Image
+                className="w-full h-full object-fit rounded-md"
+                src={item.img}
+                width={314}
+                height={108}
+                alt={item.title}
+              />
+            </figure>
+            <h2 className="text-2xl font-semibold my-5">{item.title}</h2>
+            <div className="flex items-center justify-between text-[#FF3811]">
+              <p className="text-xl font-semibold">Price: ${item.price}</p>
+              <Link href={`/services/${item._id}`}>
+                <BsArrowRight className="size-6" />
+              </Link>
+            </div>
+          </div>
         ))}
       </div>
     </div>
